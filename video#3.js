b8952222f2680c03e9cbfbb5f63cf11ad19c13f4
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-07-31 02:58:25 +0500

    update

---
+++ video#3.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3)  // we using node js, not browser
+
+console.log(5 +  
+    3);   // this type of spacing not allowed
+
+console.log("Ayyaz");
+
+let name = "ayyaz"
+let age = 16
+let state;
+
+// datatype
+
+// number =
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = not value defined 
+// symbol = unique
+
+// object
+
+console.log(typeof null); // output is object
+console.log(typeof undefined); // output is undefined
