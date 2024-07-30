2836e6910c8b36fd862c7fff1c33834d19c815ea
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-07-31 04:21:37 +0500

    update

---
+++ video#6.js
@@ -0,0 +1,19 @@
+/***** Comparison of datatypes *****/
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// === strict check
+
+// clean code is very very importent
