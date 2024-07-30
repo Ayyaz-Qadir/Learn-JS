363a37ff18f99f1f9ab378fd5b30ee1de6d83906
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-07-31 02:23:46 +0500

    update

---
+++ video#2.js
@@ -0,0 +1,13 @@
+const accountId = 1973923;
+let accountEmail = "abc@cde.com";
+var accountPassword = "12345";
+accountCity = "Lahore";
+let accountState;  // not assigned value it will show undefine
+
+// accountId = 2 // conts changing not allowed
+accountEmail = "def@gmail.com";
+accountPassword = "67890";
+accountCity = "Karachi";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
