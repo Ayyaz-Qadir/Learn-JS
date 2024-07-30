a2b446af6e1d0cc4d713df90bd68ceeebef27a57
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-07-31 04:34:17 +0500

    Update

--- video7.js
+++ video7.js
@@ -1,2 +1,11 @@
 /***** Data types of javascript summary *****/
 
+
+// Primitive Datatypes
+
+//   7-types : 1.String, 2.Number, 3.Boolean, 4.null, 5.undefined, 6.Symbol, 7.BigInt
+
+
+//  Reference (Non-Primitive)
+
+//   Array, Objects, Functions
