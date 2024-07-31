9749632c8b1b60ebf7be5110ed56c5144ed4bfd3
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-07-31 12:34:22 +0500

    update

---
+++ video#9.js
@@ -0,0 +1,40 @@
+/****** Strings in Javascript ******/ 
+
+const name = "ayyaz-qadir ";
+const repoCount = 15;
+
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo on github is ${repoCount}.`);  // use back tick 
+
+const gameName = new String("Ayyaz");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("a"));
+
+// const newString = gameName.substring(0, 5);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+console.log();
+
+const str1 = "  Ayyaz ";
+// console.log(str1);
+// console.log(str1.trim());
+
+const url =  "https://ayyaz.com/ayyaz%20abcdef"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('ayyaz'));
+
+const variable = "ayyaz-qadir-aq ";
+console.log(variable.split("-"));
+
+
