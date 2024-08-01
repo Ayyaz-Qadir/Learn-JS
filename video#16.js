2cfc694a0c889f0195a947ca3f6dbf49899f3145
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-08-01 22:18:43 +0500

    Object(p#2) De-Structure & JSON API intro

---
+++ video#16.js
@@ -0,0 +1,35 @@
+/****** Object de-structure and JSON API intro ******/
+
+/*----- Object de-structure -----*/ 
+
+const course = {
+    coursename: "JavaScript Bytwise",
+    price: "599",
+    courseInstructor: "Ahamd"
+}
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
+ /*****
+  * //in react de structuring
+
+  * const navbar = ({company}) => {
+  
+  * } 
+
+  * navbar(company = "Ayyaz Company")
+ *****/
+
+
+ /*----- JSON API intro -----*/ 
+
+ // it is not object.object has a name.like up examples. it is json.
+// {
+//     "name": "AQ",
+//     "courseName": "JS series",
+//     "coursePrice": "free"
+// }
+
+ 
