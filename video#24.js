1eb195fa0cc25603a3cb581f1c2f4058d1f7acc2
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-08-02 23:48:36 +0500

    While & Do-While Loop

---
+++ video#24.js
@@ -0,0 +1,25 @@
+/******  While & Do-While Loop  ******/
+
+let index = 0
+// while (index <= 10) {
+//     // console.log(`Value of index is ${index}`);
+//     index = index  + 2;
+// }
+
+let myArray = ['flash', 'batman', 'superman'];
+
+// let i = 0;
+// while(i < myArray.length){
+//     i++
+//     console.log(`Hero number ${i} is ${myArray[i]}`);
+//     i--
+//     i++;
+// }
+
+
+// let score = 11;
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
