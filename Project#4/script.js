4dce5695ba2cd01bb03f23aa1e41de254440cdb4
ayyaz-qadir <ayyazqadir05@gmail.com>
2024-08-06 04:56:25 +0500

    Project#4 (Guess Number)

---
+++ Project#4/script.js
@@ -0,0 +1,36 @@
+const previous_guesses = document.querySelector(".guesses");
+const guessRemaining = document.querySelector(".lastResult");
+const guess = document.querySelector("#guess");
+const form = document.querySelector(".form");
+let count = 10;
+
+const random = Math.round(Math.random() * 100 + 1);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputValue = parseInt(document.querySelector("#guessField").value);
+  if (count >= 0) {
+
+    if (inputValue < random && inputValue <= 100) {
+
+      guess.innerHTML = `Sorry! Smaller Number`;
+      previous_guesses.innerHTML = inputValue;
+
+    } else if (inputValue > random && inputValue <= 100) {
+
+      guess.innerHTML = `Sorry! Larger Number`;
+      previous_guesses.innerHTML = inputValue;
+
+    } else if (inputValue === random) {
+
+      guess.innerHTML = "Congratulation! You Gussed the Number";
+      previous_guesses.innerHTML = inputValue;
+      
+    } else {
+      guess.innerHTML = "Ops! You Enter Invalid Number";
+      previous_guesses.innerHTML = inputValue;
+    }
+    guessRemaining.innerHTML = count--;
+  }
+});
